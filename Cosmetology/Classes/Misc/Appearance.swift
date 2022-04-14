350367d6b83b6bdad1b3ec15877ea1e0dad5e16d
akonst17 <akonst17@gmail.com>
2022-04-14 16:41:21 +0300

    upgradge; build 6

--- Cosmetology/Classes/Misc/Appearance.swift
+++ Cosmetology/Classes/Misc/Appearance.swift
@@ -18,4 +18,8 @@ struct Appearance {
     static let progress1Color = UIColor(integralRed: 138, green: 59, blue: 88)
     static let progress2Color = UIColor(integralRed: 252, green: 169, blue: 164)
     static let progress3Color = UIColor(integralRed: 148, green: 185, blue: 213)
+    
+    static let successColor = UIColor(integralRed: 46, green: 190, blue: 161)
+    static let errorColor = UIColor(integralRed: 255, green: 115, blue: 99)
+    static let warningColor = UIColor(integralRed: 255, green: 155, blue: 38)
 }
--- Cosmetology/Classes/Service/Stats/Share/StatsShareManager.swift
+++ Cosmetology/Classes/Service/Stats/Share/StatsShareManager.swift
@@ -16,7 +16,7 @@ final class StatsShareManager {
 // MARK: Public
 extension StatsShareManager {
     func sharedContainerURL() -> URL? {
-        FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.cosmetology.widgets")
+        FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.cosmetology.widget")
     }
     
     @discardableResult
